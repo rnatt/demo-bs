17a4fc7e23260cccca667e67a7839812d6f65fb7
rnatt <r.nattavara@gmail.com>
2019-04-16 13:15:06 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardsComponent } from './cards/cards.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopicComponent } from './topic/topic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardsComponent,
+    FooterComponent,
+    TopicComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  tarray(n: number): any[] {
+    return Array(n);
+  }
+
+}
